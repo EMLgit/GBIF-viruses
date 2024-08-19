2d89fb2fb67a80347487b03f4ab4b00def46191f
Lizzie Lombardi <lizzie.lombardi@gmail.com>
2024-08-19 11:43:32 -0500

    initial push with basic exploration code

---
+++ Pisuvaricota_ExploreGBIFdata.R
@@ -0,0 +1,82 @@
+install.packages("finch", type = "source")
+
+#library
+library(finch)
+
+#Load data
+
+df <- simple_read("/Users/elizabethlombardi/Desktop/Research/ESIIL biotic interactions group/GBIF viruses/0072899-240626123714530.zip")
+
+
+#parse the zipped DwC archive 
+dwc.df <- dwca_read("/Users/elizabethlombardi/Desktop/Research/ESIIL biotic interactions group/GBIF viruses/Pisuviricota_INSDC Host Organisms DB/0072899-240626123714530.zip", read = TRUE)
+
+dwc.df$files
+
+
+
+#Try with Joel Nitta's dwctaxon package
+install.packages("dwctaxon")
+
+library(dwctaxon)
+library(readr)
+library(tibble)
+library(dplyr)
+
+
+occs <- read_tsv("/Users/elizabethlombardi/Desktop/Research/ESIIL biotic interactions group/GBIF viruses/Pisuviricota_INSDC Host Organisms DB/0072899-240626123714530/occurrence.txt")
+
+#validate
+validation_occs <- dct_validate(occs, on_fail = "summary")
+validation_occs
+
+
+#subset full occurrence dataframe just for quick exploration
+
+cols <- c("gbifID", "basisOfRecord", "occurrenceID", "associatedSequences", "associatedTaxa", "occurrenceRemarks", "eventDate", "year", 
+          "countryCode", "locality", "decimalLatitude", "decimalLongitude", "taxonID", "scientificName", "kingdom", "phylum", "class", "order", "family", "genus")
+
+occs.sub <- occs %>%
+  select(all_of(cols))
+
+
+#explore the data a bit
+table(occs.sub$genus)
+
+
+#table of associated taxa; the taxa are not standardized at all. This will be an issue.
+host.table <- occs.sub %>%
+  count(associatedTaxa) %>%
+  arrange(desc(n))
+
+#how many rows don't have associatedTaxa values?
+sum(is.na(occs.sub$associatedTaxa)) #zero?! nice
+
+
+
+#MAPPING?
+#{r}
+library(sf)
+library(dplyr)
+library(rnaturalearth)
+library(ggplot2)
+library(countrycode)
+
+# Create world map in Robinson projection
+countries <- ne_countries() %>%
+  st_transform("ESRI:54030")
+
+#spatial transformation of df
+occs.sub.sf <- occs.sub %>% 
+  st_as_sf(coords=c("decimalLongitude", "decimalLatitude"), crs = 4326) %>%
+  st_transform("ESRI:54030")
+
+
+#basic map
+ggplot() +
+  geom_sf(data = countries, fill = "slategrey", color = "gray") +
+  geom_sf(data = occs.sub.sf, color = "black", size = 1) +
+  coord_sf(crs = "+proj=robin") +
+  theme_minimal() +
+  labs(title = "Pisuviricota occurrences from INSDC Host Organisms DB")
+
